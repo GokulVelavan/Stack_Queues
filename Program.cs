f40018805f2f9a2ad9fb8100a6aeeb7552898f2f
GokulVelavan <gokulvelavans@gmail.com>
2021-11-10 22:39:06 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack and queues");
+            Console.ReadLine();
+        }
+    }
+}
